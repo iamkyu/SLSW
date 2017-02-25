f7d202ff033770cb6c26ba70cc9e8096f4121631
kjnam <kyujnam@gmail.com>
2017-02-25 10:45:04 +0900

    refactor: guess_number.rb

---
+++ ruby/day01/guess_number.rb
@@ -0,0 +1,31 @@
+class GuessNumber
+    attr_accessor :max, :num, :line
+
+    def initialize
+        @max = 50
+        @num = rand(max)+1
+        @line = @num+1
+    end
+
+    def prompt
+        print "Enter your number between 0 and #{max} : "
+        @line = gets.to_i
+        answer_check
+    end
+
+    def play
+        until @num == @line do
+            prompt
+        end
+    end
+
+    def answer_check
+        compare = "bigger than"  if @num > @line
+        compare = "smaller than" if @num < @line
+        compare = "correct." if @num == @line
+        puts "the answer is #{compare}  your input. : #{line}"    
+    end
+
+end
+
+GuessNumber.new.play
